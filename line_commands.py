25af61ea28d05253dc7f1afcd9eb5c58922a60a9
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-07-12 18:51:24 -0600

    Add files via upload

---
+++ line_commands.py
@@ -0,0 +1,131 @@
+from talon import Module, actions, clip
+
+
+mod = Module()
+
+line_limit = mod.setting(
+    'unsupported_application_programming_line_limit',
+    type = int,
+    default = 1000,
+    desc = 'The limit of how far the line jumping feature is willing to jump. Prevents the commands from accidentally taking an extremely long time due to accidentally dictating a gigantic number'
+)
+
+clipboard_operation_delay = mod.setting(
+    'unsupported_clipboard_operation_delay',
+    type = int,
+    default = 200,
+    desc = 'How long the unsupported application programming commands should pause when doing copying and pasting while preserving the clipboard'
+)
+
+
+
+@mod.action_class
+class Actions:
+    def unsupported_go_to_line(line_number: int):
+        '''Goes to the specified line in most unsupported applications'''
+        if line_number_below_limit(line_number): 
+            actions.edit.file_start()
+            if line_number > 1:
+                for iteration in range(0, line_number - 1):
+                    actions.edit.down()
+    def unsupported_select_lines(starting_line_number: int, final_line_number: int):
+        '''Selects the specified lines in most unsupported applications'''
+        if not valid_line_range(starting_line_number, final_line_number):
+            return 
+        actions.user.unsupported_go_to_line(starting_line_number)
+        for iteration in range(starting_line_number, final_line_number + 1):
+            actions.edit.extend_down()
+    def unsupported_select_entire_line():
+        '''Selects the entire line in most unsupported applications'''
+        actions.edit.line_end()
+        actions.edit.extend_line_start()
+        actions.edit.extend_line_start()
+
+    def unsupported_indent_lines_in_range(start: int, final: int):
+        '''Indents the specified lines in most applications'''
+        def indentation_action():
+            actions.edit.line_start()
+            actions.key('tab')
+        perform_action_at_each_line(start, final, indentation_action)
+    def unsupported_reduce_indentation_from_line_start():
+        '''Reduces the indentation assuming that the cursors at the line's start in most applications'''
+        actions.edit.extend_right()
+        actions.edit.delete() 
+    def unsupported_deindent_lines_in_range(start: int, final: int, times: int):
+        '''Reduces the indentation of the specified lines in most applications'''
+        def less_indentation_action():
+            for iteration in range(times):
+                actions.user.unsupported_reduce_indentation_from_line_start()
+        perform_action_at_each_line(start, final, less_indentation_action)
+    
+    def unsupported_comment_current_line():
+        '''Comments the current line in most applications'''
+        actions.user.unsupported_select_entire_line()
+        actions.edit.left()
+        actions.user.code_comment_line_prefix()
+
+
+    def unsupported_clone_line():
+        '''Clones the current line in most applications'''
+        line_text = get_line()
+        actions.edit.right()
+        actions.key('enter')
+        paste_text(line_text)
+    def unsupported_drag_line_down():
+        '''Drags the line down one line in most unsupported applications'''
+        text = cut_line()
+        actions.edit.delete()
+        actions.edit.down()
+        start_line_below()
+        paste_text(text)
+    def unsupported_drag_line_up():
+        '''Drags the line up one line in most applications'''
+        bottom_text = cut_line()
+        actions.edit.delete()    
+        actions.edit.line_start()
+        top_text = cut_line()
+        paste_text(bottom_text)
+        actions.key('enter')
+        actions.user.unsupported_select_entire_line()
+        paste_text(top_text)
+
+    
+
+def line_number_below_limit(number):
+    return number <= line_limit.get()
+
+def start_line_below():
+    actions.edit.line_end()
+    actions.key('enter')
+
+def perform_action_at_each_line(starting_line_number, final_line_number, action):
+    if valid_line_range(starting_line_number, final_line_number):
+        actions.user.unsupported_go_to_line(starting_line_number)
+        for iteration in range(starting_line_number, final_line_number + 1):
+            action()
+            actions.edit.down()
+
+def valid_line_range(start, final):
+    return  line_number_below_limit(start) and line_number_below_limit(final) and start <= final
+
+def paste_text (text: str):
+    with clip.revert():
+        clip.set_text(text)
+        actions.edit.paste()
+        wait_long_enough_to_let_clipboard_revert_properly()
+def cut_line():
+    text = get_line()
+    actions.edit.delete()
+    return text
+
+def get_line():
+    '''Returns the current line'''
+    with clip.revert():
+        actions.user.unsupported_select_entire_line()
+        actions.edit.copy()
+        wait_long_enough_to_let_clipboard_revert_properly()
+        result = clip.text()
+    return result
+
+def wait_long_enough_to_let_clipboard_revert_properly():
+	actions.sleep(f'{clipboard_operation_delay.get()}ms')
---
+++ line_commands.talon
@@ -0,0 +1,115 @@
+#this file uses modified and unmodified code from the talon voice community repository
+#located here: https://github.com/knausj85/knausj_talon
+#under the following license:
+#MIT License
+#
+#Copyright (c) 2021 Jeff Knaus, Ryan Hileman, Zach Dwiel, Michael Arntzenius, and others
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+#
+#(end of the community repository license)
+
+
+not tag: user.line_commands
+and tag: /(code|^(user.protobuf|user.go|user.markdown|user.batch)$)/i
+header requires that the regular knausj line commands are not enabled
+#and a tag with the word code in it (case insensitive) or one of the
+#tags for languages that knausj currently supports at the time that this
+#comment was written that do not use any tags with the word code in it
+#using force <language name> with knausj should enable these commands
+#without the user.line_commands tag for most knausj supported languages
+-
+#these two commands were taken from the community repository without modification
+lend: edit.line_end()
+bend: edit.line_start()
+
+go <number> :
+    user.unsupported_go_to_line(number)
+go <number> end:
+    user.unsupported_go_to_line(number)
+    edit.line_end()
+
+comment [line] <number>:
+    user.unsupported_go_to_line(number)
+    user.unsupported_comment_current_line()
+
+
+clear [line] <number>:
+    user.unsupported_go_to_line(number)
+    edit.select_line()
+    edit.delete()
+clear <number> until <number>:
+    user.unsupported_select_lines(number_1, number_2)
+    edit.delete()
+
+(select|cell|sell) <number>:
+    user.unsupported_go_to_line(number)
+    edit.select_line()
+(select|cell|sell) <number> until <number>:
+    user.unsupported_select_lines(number_1, number_2)
+
+copy [line] <number>:
+    user.unsupported_go_to_line(number)
+    edit.select_line()
+    edit.copy()
+copy <number> until <number>:
+    user.unsupported_select_lines(number_1, number_2)
+    edit.copy()
+
+cut [line] <number>:
+    user.unsupported_go_to_line(number)
+    edit.select_line()
+    edit.cut()
+cut <number> until <number>:
+    user.unsupported_select_lines(number_1, number_2)
+    edit.cut()
+
+(paste|replace) <number> until <number>:
+    user.unsupported_select_lines(number_1, number_2)
+    edit.paste()
+
+tab that: key(tab)
+tab [line] <number>:
+    user.unsupported_go_to_line(number)
+    key(tab)
+tab <number> until <number>:
+    user.unsupported_indent_lines_in_range(number_1, number_2)
+retab that:
+    user.unsupported_select_entire_line()
+    edit.left()
+    edit.extend_right()
+    edit.delete()
+retab [line] <number>: 
+    user.unsupported_go_to_line(number)
+    user.unsupported_reduce_indentation_from_line_start()
+retab <number> until <number> [<user.ordinals>]:
+    user.unsupported_deindent_lines_in_range(number_1, number_2, ordinals or 1)
+
+
+drag [line] down:
+    user.unsupported_drag_line_down()
+drag [line] up:
+    user.unsupported_drag_line_up()
+
+
+clone (line|that):
+    user.unsupported_clone_line()
+    
+
+
